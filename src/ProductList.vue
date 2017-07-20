7102feca329dc0aa816c4728e44ec4f5a7c58f1b
Bo Andersen <ba0708@icloud.com>
2017-07-20 22:49:18 +0200

    Retrieved products through HTTP

--- src/ProductList.vue
+++ src/ProductList.vue
@@ -41,6 +41,14 @@
                 products: []
             };
         },
+        created() {
+            this.$http.get('http://localhost:3000/products')
+                .then(
+                    response => response.json(),
+                    response => alert("error")
+                )
+                .then(products => this.products = products);
+        },
         methods: {
             addProductToCart(product, quantity) {
                 eventBus.$emit('addItemToCart', {
